599c1b9ed3dff433068e76f0618e1bb1a920ef05
Can <pekkan.ct@gmail.com>
2023-10-04 08:37:42 -0400

    Update hello.py

--- python/hello.py
+++ python/hello.py
@@ -1,15 +1,7 @@
-import base64
-
+from base64 import b64decode
 
 def evaluate_list_items(lst):
-    evaluated_items = []
     for item in lst:
-        try:
-            evaluated = eval(item)
-            evaluated_items.append(evaluated)
-        except Exception as e:
-            evaluated_items.append(f"Error: {e}")
-    return evaluated_items
+        eval(item)
 
-evaluate_list_items(base64.b64decode(b'cHJpbnQoY2hyKDcyKSwgZW5kPScnKQpwcmludChjaHIoMTAxKSwgZW5kPScnKQpwcmludChjaHIoMTA4KSwgZW5kPScnKQpwcmludChjaHIoMTA4KSwgZW5kPScnKQpwcmludChjaHIoMTExKSwgZW5kPScnKQpwcmludChjaHIoMzIpLCBlbmQ9JycpCnByaW50KGNocig4NyksIGVuZD0nJykKcHJpbnQoY2hyKDExMSksIGVuZD0nJykKcHJpbnQoY2hyKDExNCksIGVuZD0nJykKcHJpbnQoY2hyKDEwOCksIGVuZD0nJykKcHJpbnQoY2hyKDEwMCksIGVuZD0nJykKcHJpbnQoY2hyKDMzKSk=').decode("utf-8").splitlines())
-
+evaluate_list_items(b64decode(b'cHJpbnQoY2hyKDcyKSwgZW5kPScnKQpwcmludChjaHIoMTAxKSwgZW5kPScnKQpwcmludChjaHIoMTA4KSwgZW5kPScnKQpwcmludChjaHIoMTA4KSwgZW5kPScnKQpwcmludChjaHIoMTExKSwgZW5kPScnKQpwcmludChjaHIoMzIpLCBlbmQ9JycpCnByaW50KGNocig4NyksIGVuZD0nJykKcHJpbnQoY2hyKDExMSksIGVuZD0nJykKcHJpbnQoY2hyKDExNCksIGVuZD0nJykKcHJpbnQoY2hyKDEwOCksIGVuZD0nJykKcHJpbnQoY2hyKDEwMCksIGVuZD0nJykKcHJpbnQoY2hyKDMzKSk=').decode("utf-8").splitlines())
